2a6f1c55e83489113ed779f1cec7d3e1d15099b5
Artem Kandaurov <admin@kandaart.com>
2017-05-19 03:22:30 +0200

    Initial Commit

---
+++ ICONResizing/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ICONResizing
+//
+//  Created by Artem Kandaurov on 19.05.17.
+//  Copyright © 2017 Artem Kandaurov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ICONResizing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ICONResizing
+//
+//  Created by Artem Kandaurov on 19.05.17.
+//  Copyright © 2017 Artem Kandaurov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
